bea7d1465c007073fdd80fb1961e012c28fe125d
Nik Meyn <meyn.nik@gmail.com>
2019-04-08 13:08:12 -0600

    First host to heroku

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function(req, res, next) {
 
 // serves up static files from the public folder. 
 app.use(express.static('client'));
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/src')));
 
 // setup express middleware
 app.use(parser.json());
@@ -269,6 +269,6 @@ app.post('/profile', function (req, res) {
 })
 	})
 
-app.get('*', (req, res) => {
-	 res.sendFile(path.join(__dirname+'/client/src/App.js'));
-});
+	// app.get('*', (req, res) => {
+  //   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+	// });
